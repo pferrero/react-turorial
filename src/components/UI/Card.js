e09331c8d7f40116d6cf281ff71645a78b35cac4
Pablo Ferrero <ferreronicolas58@gmail.com>
2021-07-15 18:22:27 -0300

    Card and folders. - Card component to wrap other components with similar styles. - Deparation of components and styles into different folders.

---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+/*
+    Card component with rounded corners.
+*/
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;

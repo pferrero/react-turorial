bc773d47fac54e5bf1bb2b8ad800d470e21c2164
Pablo Ferrero <ferreronicolas58@gmail.com>
2021-07-17 23:26:00 -0300

    Expense form to add new expenses.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseList from "./components/Expenses/ExpenseList";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,6 +27,7 @@ function App() {
   return (
     <div>
       <h2>Expenses</h2>
+      <NewExpense />
       <ExpenseList expenses={expenses} />
     </div>
   );

749e80fbeb401ac54b491d981f237637f1530f39
Pablo Ferrero <ferreronicolas58@gmail.com>
2021-08-01 20:23:28 -0300

    Food ordering app init

---
+++ food-ordering-project/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import headerImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+/* Header of the page with the cart and the image */
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={headerImage} alt="Table with meals." />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
---
+++ food-ordering-project/src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious food</h2>
+      <p>paragraph</p>
+      <p>paragraph 2</p>
+    </section>
+  );
+};
+
+export default MealsSummary;

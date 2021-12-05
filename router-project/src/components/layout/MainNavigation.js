f5baac74882faa05e010b8a290dedfee15235bf5
Pablo Ferrero <ferreronicolas58@gmail.com>
2021-12-05 16:46:26 -0300

    react router 5

---
+++ router-project/src/components/layout/MainNavigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Quotes</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/quotes">
+              Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/newQuote">
+              New Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default React.memo(MainNavigation);
---
+++ router-project/src/pages/NotFount.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <p>404 Page not found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
